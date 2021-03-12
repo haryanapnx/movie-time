dbdab24b1ad86b968c28a2c291e83100fa1d8409
Haryana <hary.vai@gmail.com>
2021-03-12 08:19:47 +0700

    Update movieAction.js

--- src/redux/actions/movieAction.js
+++ src/redux/actions/movieAction.js
@@ -10,41 +10,49 @@ export function setLoading(bool) {
 
 export const getMovies = () => async dispatch => {
   dispatch(setLoading(true));
-  const res = await dispatch(
-    apiCall({
-      method: "get",
-      url: `/movie/now_playing?api_key=e851c47c40552466a1a415867974f98a`
-    })
-  );
-  if (res) {
-    const { results } = res.data;
-    dispatch(setLoading(false));
-    return dispatch({
-      type: constants.MOVIES_GET,
-      payload: results
-    });
-  } else {
+  try {
+
+    const res = await dispatch(
+      apiCall({
+        method: "get",
+        url: `/movie/now_playing?api_key=e851c47c40552466a1a415867974f98a`
+      })
+    );
+    if (res) {
+      const { results } = res.data;
+   
+      return dispatch({
+        type: constants.MOVIES_GET,
+        payload: results
+      });
+    } 
+  } catch (err) {
+    console.error(err)
+  } finally {
     dispatch(setLoading(false));
   }
-  //some function
 };
+
 export const searchMovies = (query) => async dispatch => {
   dispatch(setLoading(true));
-  const res = await dispatch(
-    apiCall({
-      method: "get",
-      url: `/search/movie?api_key=e851c47c40552466a1a415867974f98a&query=${query}`
-    })
-  );
-  if (res) {
-    const { results } = res.data;
-    dispatch(setLoading(false));
-    return dispatch({
-      type: constants.MOVIES_SET,
-      payload: results
-    });
-  } else {
+
+  try {
+    const res = await dispatch(
+      apiCall({
+        method: "get",
+        url: `/search/movie?api_key=e851c47c40552466a1a415867974f98a&query=${query}`
+      })
+    );
+    if (res) {
+      const { results } = res.data;
+      return dispatch({
+        type: constants.MOVIES_SET,
+        payload: results
+      });
+    }
+  }catch (err){
+    console.error(err)
+  }finally{
     dispatch(setLoading(false));
   }
-  //some function
 };
